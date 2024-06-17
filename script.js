59f4a418a6f925e3f2d67db6015689eda6446e96
guiiBecker <guilhermegbecker@gmail.com>
2024-06-17 15:26:39 -0300

    Refactor: site polish

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     productCountSelect.addEventListener('change', updateProductDisplay);
 
-    // Initialize display
+    
     updateProductDisplay();
 });
 // -------------------------------------- //
